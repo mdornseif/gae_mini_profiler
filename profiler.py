07a4204afdd324020f4a58f41610c1a9175147fe
Maximillian Dornseif <md@hudora.de>
2012-01-15 00:21:59 +0100

    Use Cookies to send RequestID to Browser.
    This allows to use completely static HTML on the frontend
    and thus much easier integration into other template
    engines than django.

--- profiler.py
+++ profiler.py
@@ -349,6 +349,7 @@ class ProfilerWSGIMiddleware(object):
                 # Append headers used when displaying profiler results from ajax requests
                 headers.append(("X-MiniProfiler-Id", request_id))
                 headers.append(("X-MiniProfiler-QS", environ.get("QUERY_STRING")))
+                headers.append(('Set-Cookie', 'MiniProfilerId=%s; Max-Age=10' % request_id))
 
                 return start_response(status, headers, exc_info)
 
